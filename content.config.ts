4489ebaacd8c79a3adeb4e1b246aa3c359f45801
freezyh <2294963715@qq.com>
2026-04-01 15:14:40 +0800

    blogs

--- content.config.ts
+++ content.config.ts
@@ -4,7 +4,7 @@ export default defineContentConfig({
   collections: {
     blog: defineCollection({
       type: "page",
-      source: "blog/**/*.md",
+      source: "blogs/**/*.md",
       schema: z.object({
         date: z.string(),
         year: z.string(),
